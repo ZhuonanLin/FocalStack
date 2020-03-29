42082c8dd197f08c5b0f0792da521e05b15ef2f5
linzn <zhl383@eng.ucsd.edu>
2020-03-28 22:38:28 -0700

    zl - focalStack class

---
+++ focalStack.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+import numpy as np
+import cv2
+
+class focalStack:
+
+    def __init__(self, inputDir = './Input/', detectorMethod = 'ORB'):
+        self.inputDir = inputDir
+        self.detectorMethod = detectorMethod
+        self.images = [cv2.imread(self.inputDir + imName) for imName in sorted(os.listdir(self.inputDir)) 
+            if imName.split('.')[-1] in ["jpg", "jpeg", "png"] ]
+        self.alignedImages = []
+        self.mergedImage = None
+        if not os.path.exists('./Output/'):
+            os.mkdir('./Output/')
+        else:
+            shutil.rmtree('./Output/*')
+
+
